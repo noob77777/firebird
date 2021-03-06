a805d065cc0ae14cc42f5af8fee14ec6aff82cd1
root <root@instance-main.us-central1-a.c.firebird-backend.internal>
2021-03-06 15:06:00 +0000

    Added: production build

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -1,5 +1,5 @@
-export const SERVER_PORT_HTTP: number = 8080;
-export const SERVER_PORT_HTTPS: number = 8443;
+export const SERVER_PORT_HTTP: number = 80;
+export const SERVER_PORT_HTTPS: number = 443;
 export const REDIS_HOST: string = "127.0.0.1";
 export const REDIS_PORT: number = 6379;
 
